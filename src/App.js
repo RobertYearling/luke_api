c5fb4701a18658d16db0ae5c4503de33c5724760
Robert Yearling <laserguy@mac.com>
2021-01-15 17:38:11 -0600

    Files updated

--- src/App.js
+++ src/App.js
@@ -4,41 +4,36 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import People from './components/People';
 import Planets from './components/Planets';
-
 function App() {
   const [selected, setSelected] = useState("people");
   const [id, setId] = useState("1");
-
   const onSubmitHandler = e =>{
       e.preventDefault();
   }
-
   let link = `/${selected}/${id}`;
-
   return (
     <>
-    <div className="App container m-600">
-      <h3>Search for:</h3>
-      <div className="row">
-        <form onSubmit={onSubmitHandler} className="form-control">
-          <select className="form-control w-25" onChange={ e => setSelected(e.target.value)}>
-              <option value="people">People</option>
-              <option value="planets">Planets</option>
-          </select>
-          <label>ID: </label>
-          <input type="number" className= "id" onChange={ e => setId(e.target.value)} />
-          <Link to={link}><button>Search</button></Link>
-        </form>
+      <div className="App container-fluid">
+        <div className="row">
+          <h3>Search for:</h3>
+          <form onSubmit={onSubmitHandler} className="">
+            <select className="form-control w-50" onChange={ e => setSelected(e.target.value)}>
+                <option value="people">People</option>
+                <option value="planets">Planets</option>
+            </select>
+            <label>ID: </label>
+            <input type="number" className= "id" onChange={ e => setId(e.target.value)} />
+            <Link to={link}><button className="btn-primary">Search</button></Link>
+          </form>
+        </div>
+        <div className="row mt-50">
+          <Router>
+            <Planets path="/planets/:id" />
+            <People path="/people/:id" />
+          </Router>
+        </div>
       </div>
-      <div className="row mt-50">
-        <Router>
-          <Planets path="/planets/:id" />
-          <People path="/people/:id" />
-        </Router>
-      </div>
-    </div>
     </>
   );
 };
-
 export default App;
